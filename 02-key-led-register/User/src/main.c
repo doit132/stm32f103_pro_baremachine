045a744bd654ba8541b2438be0c46725fa93320e
doit132 <wouitc132@qq.com>
2023-12-11 21:46:17 +0800

    02-key-led-register

---
+++ 02-key-led-register/User/src/main.c
@@ -0,0 +1,48 @@
+
+void delay(int d)
+{
+	while (d--)
+		;
+}
+
+int main()
+{
+	unsigned int *pReg;
+	unsigned int *pRegA;
+	unsigned int *pRegB;
+
+	/* 使能 GPIOB, GPIOA */
+	pReg = (unsigned int *)(0x40021000 + 0x18);
+	*pReg |= (1 << 3) | (1 << 2);
+
+	/* 设置 GPIOB0 为输出引脚 */
+	pRegB = (unsigned int *)(0x40010C00 + 0x00);
+	*pRegB |= (1 << 0);
+
+	/* 设置 GPIOA0 为输入引脚 */
+	pRegA = (unsigned int *)(0x40010800 + 0x00);
+	*pRegA &= ~(3);	     /* mode0 = 0b00 */
+	*pRegA &= ~(3 << 2); /* cnf0 = 0b00 */
+	*pRegA |= (1 << 2);  /* cnf0 = 0b01 */
+
+	/* GPIOB output data register */
+	pRegB = (unsigned int *)(0x40010C00 + 0x0C);
+
+	/* GPIOA input data register */
+	pRegA = (unsigned int *)(0x40010800 + 0x08);
+
+	while (1) {
+		/* 读取GPIOA input data register */
+
+		if ((*pRegA & (1 << 0)) == 0) /* KEY1被按下 */
+		{
+			/* 设置GPIOB0输出0 */
+			*pRegB &= ~(1 << 0);
+		} else {
+			/* 设置GPIOB0输出1 */
+			*pRegB |= (1 << 0);
+		}
+	}
+
+	return 0;
+}
